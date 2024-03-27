ba046aa30dbcf361d5da56433e71e80da03d8678
Euwen Meredith <euwenmeredith@outlook.com>
2024-03-27 09:54:37 -0700

    starting excersise

---
+++ src/AddingIntegers.java
@@ -0,0 +1,11 @@
+public class AddingIntegers
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        System.out.println(a+b+c);
+    }
+}
