6393dd6e10a428c86756c41e9e3d5ba86cf0fea2
Euwen Meredith <euwenmeredith@outlook.com>
2024-03-28 10:22:52 -0700

    new excersise

--- Chap22_EX2_MilesPerGallon/src/Main.java
+++ Chap22_EX2_MilesPerGallon/src/Main.java
@@ -2,7 +2,41 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int a = 0;
-        int b=2;
+        /*
+        Write a program that calculates miles per gallon for a list of cars.
+
+         The data for each car consists of initial odometer reading,
+
+          final odometer reading,
+
+          and number of gallons of gas.
+
+          The user signals that there are no more cars by
+          entering a negative initial odometer reading.
+
+        Miles Per Gallon Program
+        Initial miles:
+        15000
+        Final miles:
+        15250
+        Gallons
+        10
+        Miles per Gallon: 25.0
+
+        Initial miles:
+        107000
+        Final miles:
+        107450
+        Gallons
+        15
+        Miles per Gallon: 30.0
+
+        Initial miles:
+        -1
+        bye
+         */
+
+
+
     }
 }
